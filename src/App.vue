dff638f6dd36dbf8382920074df2b5d64c81eccf
Melnyk Mykyta <elder.od13@gmail.com>
2024-05-28 17:07:48 +0200

    Changes at visoma

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,194 @@
+<script setup>
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import { computed, ref } from 'vue';
+import axios from 'axios';
+
+console.log(axios.isCancel('something'));
+
+const posts = ref()
+
+function getPosts() {
+  axios.get('https://jsonplaceholder.typicode.com/posts/')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+
+      // Set posts variable
+      posts.value = response.data
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+function clearPosts() {
+  posts.value = undefined
+}
+
+
+
+const products = [
+  {
+    code: "1",
+    name: "Apple AirPods Basic",
+    preis: 249.00,
+    quantity: "10,000"
+  },
+  {
+    code: "2",
+    name: "Instant Pot Duo 7-in-1",
+    preis: 79.95,
+    quantity: "15,000"
+  },
+  {
+    code: "3",
+    name: "Kindle Paperwhite",
+    preis: 80.56,
+    quantity: "7,000"
+  },
+  {
+    code: "4",
+    name: "Nintendo Switch",
+    preis: 43.76,
+    quantity: "5,000"
+  },
+  {
+    code: "5",
+    name: "Echo Dot (3rd Gen)",
+    preis: 65.97,
+    quantity: "2,000"
+  },
+  {
+    code: "6",
+    name: "Fire TV Stick 4K",
+    preis: 99.99,
+    quantity: "6,500"
+  },
+  {
+    code: "7",
+    name: "Fitbit Charge 4",
+    preis: 23.87,
+    quantity: "5,000"
+  },
+  {
+    code: "8",
+    name: "LEGO Star Wars Set",
+    preis: 122.76,
+    quantity: "3,000"
+  },
+  {
+    code: "9",
+    name: "Sony WH-1000XM4",
+    preis: 976.55,
+    quantity: "11,000"
+  },
+  {
+    code: "10",
+    name: "Instant Pot Ultra",
+    preis: 76.88,
+    quantity: "9,000"
+  },
+]
+
+const selectedItem = ref()
+
+function selectItem(event) {
+  selectedItem.value = event.data
+}
+
+const isExpensive = computed(() => {
+
+  if (selectedItem.value == null) {
+    return 'Kein Produkt gewählt.'
+  }
+
+  if (selectedItem.value.preis >= 100) {
+    return true;
+  }
+
+  return false;
+})
+
+// const selectedItem2 = ref()
+
+// function selectItem2(event) {
+//   selectedItem2.value = event.data
+// }
+
+const isAlot = computed(() => {
+
+  if (selectedItem.value == null) {
+    return 'Kein Produkt gewählt.'
+  }
+
+  if (selectedItem.value.quantity >= 5000) {
+    return true;
+  }
+
+  return false;
+})
+
+
+
+
+
+</script>
+
+
 <template>
   <div id="app">
     <h1>AMAZON</h1>
 
-    <table>
+    {{ selectedItem }}<br>
+    {{ isExpensive }}<br>
+
+    <!-- {{ selectedItem2 }}<br> -->
+    {{ isAlot }}<br>
+
+    <button @click="getPosts()">Posts abrufen</button>
+
+    <button @click="clearPosts()">Posts löschen</button>
+
+    <!-- @TODO Replace div with DataTable -->
+    <div v-for="post in posts" :key="post.id">
+      <h1>{{ post.title }}</h1>
+    </div>
+
+    <div v-if="selectedItem">
+      <span v-if="isExpensive" style="color: #f00;">Teuer! ({{ selectedItem?.preis }})</span>
+      <span v-else style="color: #0f0;">Günstig! ({{ selectedItem?.preis }})</span>
+    </div>
+
+    <div v-if="selectedItem">
+      <span v-if="isAlot" style="color: #f00;">Genug! ({{ selectedItem?.quantity }})</span>
+      <span v-else style="color: #0f0;">Nicht Genug! ({{ selectedItem?.quantity }})</span>
+    </div>
+
+
+
+
+
+    <DataTable :value="products" tableStyle="min-width: 50rem" @rowDblclick="(event) => selectItem(event)">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="preis" header="Preis">
+        <template #body="{ data }">
+          ${{ data.preis }}
+        </template>
+      </Column>
+      <Column field="quantity" header="Quantity">
+        <template #body="{ data }">
+          {{ data.quantity }}
+        </template>
+      </Column>
+    </DataTable>
+
+    <!-- <table>
       <thead>
         <tr>
           <th>Produktname</th>
@@ -12,7 +198,7 @@
       </thead>
       <tbody>
         <tr>
-          <td class="highlight">Apple AirPods Pro</td>
+          <td class="highlight">Apple AirPods Basic</td>
           <td class="highlight">$249.00</td>
           <td class="highlight">10,000</td>
         </tr>
@@ -61,17 +247,15 @@
           <td class="highlight">$149.95</td>
           <td class="highlight">10,000</td>
         </tr>
-        <!-- Добавьте другие строки таблицы при необходимости -->
+       
       </tbody>
-    </table>
+    </table> -->
+
+    <!-- Добавьте другие строки таблицы при необходимости -->
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-};
-</script>
+
 
 
 <style>
@@ -86,11 +270,12 @@ export default {
 
 table {
   width: 100%;
-  border-collapse: separate; 
-  
+  border-collapse: separate;
+
 }
 
-th, td {
+th,
+td {
   border: 5px solid #00ADB5;
   text-align: left;
   padding: 8px;
@@ -101,14 +286,11 @@ th {
 }
 
 tr {
-  background-color: #EEEEEE ;
+  background-color: #EEEEEE;
 }
 
 .highlight {
-  color: #037490; /* Цвет текста для ячеек с классом "highlight" */
+  color: #037490;
+  /* Цвет текста для ячеек с классом "highlight" */
 }
-
-
 </style>
-
-
